09a4292ad8693d18b39e5c3383858ee13021a76d
Michael Croghan <mcrog.7@gmail.com>
2022-09-01 12:05:31 -0400

    Create Queue (LL Implementation)

--- src/datastructures/queues/Queue.java
+++ src/datastructures/queues/Queue.java
@@ -1,4 +1,61 @@
 package datastructures.queues;
+// Linked List Implementation
+public class Queue<T> {
 
-public class Queue {
+    private int size = 0;
+    private Node start;
+    private Node end;
+
+    public void enqueue(T value){
+        Node newNode = new Node(value);
+
+        if(size == 0){
+            start = newNode;
+            end = newNode;
+        }
+        else{
+            start.next = newNode;
+            start = newNode;
+        }
+        size++;
+    }
+
+    public Object dequeue(){
+        if (size == 0){
+            throw new RuntimeException("No objects exist in the queue!");
+        }
+        Object returnVal = end.value;
+        end = end.next;
+        size--;
+        return returnVal;
+    }
+
+    public void peek(){
+        if(size == 0){
+            System.out.println("Stack is empty!");
+        }
+        else{
+            System.out.println(end.value);
+        }
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+
+
+    private class Node<T>{
+        private T value;
+        private Node next;
+
+        public Node(T value){
+            this.value = value;
+            this.next = null;
+        }
+    }
 }
