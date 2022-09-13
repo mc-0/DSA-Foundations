5730ef5dbca0e1300e9e6dcd17e75c29e7f63d6e
Michael Croghan <mcrog.7@gmail.com>
2022-09-13 19:50:27 -0400

    Update QuickSort.java

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -1,5 +1,9 @@
 package sorting;
 
+// Best case time complexity => O(n log n). Pivot falls in the median. Won't necessarily happen (maybe randomly).
+// Worst case time complexity => O(n^2). Happens when the list is already sorted.
+// >>> Mitigate the worst case by choosing the middle element or random element as the pivot.
+// Space Complexity: Recursive solution makes use of a stack. Can be O(log n) - O(n)
 public class QuickSort {
 
     public static void sort(int[] arr, int left, int right){
