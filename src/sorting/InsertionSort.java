8fb601b2c4ed721c312e7e0ba437b4666bd9e518
Michael Croghan <mcrog.7@gmail.com>
2023-09-13 07:36:25 -0400

    extract insertion sort into own class

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,4 +1,24 @@
 package sorting;
 
+import sorting.util.Swapper;
+
 public class InsertionSort {
+
+    // Treats the first item in an array as sorted & continues to sort successive values to where they belong.
+    // Runtime: O(n^2) Quadratic || Space: O(1) || Stable Sort
+    // In the best case can be O(n) linear time complexity if dealing with a small number of inputs that are nearly sorted.
+    public static void sort(int[] a) {
+        for (int i = 1; i < a.length; i++){
+
+            int currentValue = a[i];
+            int j = i - 1;
+
+            while (j >= 0 && a[j] > currentValue){
+                Swapper.swap(a, j, j+1);
+                j--;
+
+            }
+        }
+    }
+
 }
--- src/sorting/SortingClient.java
+++ src/sorting/SortingClient.java
@@ -10,8 +10,9 @@ public class SortingClient {
 
     public static void main(String[] args) throws FileNotFoundException {
         int[] array = InitInput.populateArray("src\\inputdata\\randomInts.txt");
-        SelectionSort.sort(array);
-        System.out.println(Arrays.toString(array));
+        int[] generic = {10,8,1,3,6,9,0,2,4,7,5};
+        InsertionSort.sort(generic);
+        System.out.println(Arrays.toString(generic));
     }
 
 
