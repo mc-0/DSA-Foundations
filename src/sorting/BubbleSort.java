d3910eb4e7461baf35609ca2470475ed919db96f
Michael Croghan <mcrog.7@gmail.com>
2023-09-14 20:46:04 -0400

    set bubblesort class to single static sort method

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -4,16 +4,9 @@ import sorting.util.Swapper;
 
 public class BubbleSort {
 
-    private int[] collection;
-    private boolean sorted;
-
-    public BubbleSort(int[] collection){
-        this.collection = collection;
-        this.sorted = false;
-    }
 
     // Runtime: O(n^2) Quadratic || Space: O(1) || Stable
-    public void sort(){
+    public static void sort(int [] collection){
         int n = collection.length;
         for(int i=0; i < n; i++){
             for(int j=1; j < (n-i); j++){
@@ -23,24 +16,6 @@ public class BubbleSort {
             }
 
         }
-        this.sorted = true;
-    }
-
-    public void printList() {
-        StringBuilder sb = new StringBuilder();
-        int index = 0;
-        while (index < collection.length) {
-            if (index % 20 == 0) {
-                sb.append("\n");
-            }
-            sb.append(collection[index]).append(" ");
-            index++;
-        }
-        System.out.println(sb);
-    }
-
-    public boolean isSorted() {
-        return sorted;
     }
 
 }
