6214a4dcea689573b8874a91b71d78951920eddb
Michael Croghan <mcrog.7@gmail.com>
2023-12-11 20:35:07 -0500

    add searchTriplet

--- src/patterns/twopointers/TwoPointers.java
+++ src/patterns/twopointers/TwoPointers.java
@@ -1,6 +1,8 @@
 package patterns.twopointers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TwoPointers {
 
@@ -90,4 +92,68 @@ public class TwoPointers {
         }
         return squared;
     }
+
+    public static List<List<Integer>> allTriplatesSumToZero(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> triplets = new ArrayList<>();
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) // skip same element to avoid duplicate triplets
+                continue;
+            searchPair(arr, -arr[i], i + 1, triplets);
+        }
+
+        return triplets;
+    }
+
+    private static void searchPair(int[] arr, int targetSum, int left, List<List<Integer>> triplets) {
+        int right = arr.length - 1;
+        while (left < right) {
+            int currentSum = arr[left] + arr[right];
+            if (currentSum == targetSum) { // found the triplet
+                triplets.add(Arrays.asList(-targetSum, arr[left], arr[right]));
+                left++;
+                right--;
+                while (left < right && arr[left] == arr[left - 1])
+                    left++; // skip same element to avoid duplicate triplets
+                while (left < right && arr[right] == arr[right + 1])
+                    right--; // skip same element to avoid duplicate triplets
+            } else if (targetSum > currentSum)
+                left++; // we need a pair with a bigger sum
+            else
+                right--; // we need a pair with a smaller sum
+        }
+    }
+
+
+    public static int searchTriplet(int[] arr, int targetSum) {
+        if (arr == null || arr.length < 3)
+            throw new IllegalArgumentException();
+
+        Arrays.sort(arr);
+        int smallestDifference = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length - 2; i++) {
+            int left = i + 1, right = arr.length - 1;
+            while (left < right) {
+                // comparing the sum of three numbers to the 'targetSum' can cause overflow
+                // so, we will try to find a target difference
+                int targetDiff = targetSum - arr[i] - arr[left] - arr[right];
+                if (targetDiff == 0) //  we've found a triplet with an exact sum
+                    return targetSum; // return sum of all the numbers
+
+                // the second part of the above 'if' is to handle the smallest sum when we have
+                // more than one solution
+                if (Math.abs(targetDiff) < Math.abs(smallestDifference)
+                        || (Math.abs(targetDiff) == Math.abs(smallestDifference)
+                        && targetDiff > smallestDifference))
+                    smallestDifference = targetDiff; // save the closest and the biggest difference
+
+                if (targetDiff > 0)
+                    left++; // we need a triplet with a bigger sum
+                else
+                    right--; // we need a triplet with a smaller sum
+            }
+        }
+        return targetSum - smallestDifference;
+    }
+
 }
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -8,10 +8,8 @@ public class PracticeClient {
 
 
     public static void main(String[] args) {
-        int[] nums = {-2,-1,0,2,3};
-
-        int[] r = TwoPointers.squareSortedArray(nums);
-        System.out.println(Arrays.toString(r));
+        System.out.println(TwoPointers.allTriplatesSumToZero(
+                new int[] { -3, 0, 1, 2, -1, 1, -2 }));
     }
 
 }
