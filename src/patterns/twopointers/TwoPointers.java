af4ee7261a5a0118cce2004d714800b466ae7499
Michael Croghan <mcrog.7@gmail.com>
2023-12-13 07:14:07 -0500

    add findSubarrays (contiguous)

--- src/patterns/twopointers/TwoPointers.java
+++ src/patterns/twopointers/TwoPointers.java
@@ -2,6 +2,7 @@ package patterns.twopointers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TwoPointers {
@@ -206,6 +207,45 @@ public class TwoPointers {
                 right--; // we need a pair with a smaller sum
             }
         }
+    }
+
+    public static List<List<Integer>> findSubarrays(int[] arr, int target) {
+        // Resultant list to store all valid subarrays.
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Variable to store the product of elements in the current subarray.
+        double product = 1;
+
+        // Left boundary of the current subarray.
+        int left = 0;
+
+        // Iterate over the array using 'right' as the right boundary of the current
+        // subarray.
+        for (int right = 0; right < arr.length; right++) {
+            // Update the product with the current element.
+            product *= arr[right];
+
+            // If the product is greater than or equal to the target, slide the left
+            // boundary to the right until product is less than target.
+            while (product >= target && left < arr.length) {
+                product /= arr[left++];
+            }
+
+            // Create a temporary list to store the current subarray.
+            List<Integer> tempList = new LinkedList<>();
+
+            // Iterate from 'right' to 'left' and add all these subarrays to the result.
+            for (int i = right; i >= left; i--) {
+                // Add the current element at the beginning of the list.
+                tempList.add(0, arr[i]);
+
+                // Add the current subarray to the result.
+                result.add(new ArrayList<>(tempList));
+            }
+        }
+
+        // Return the result.
+        return result;
     }
 
 }
