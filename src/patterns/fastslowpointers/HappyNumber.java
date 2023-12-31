003b7f3617a835fb6c6406390bf6eb74f626a581
Michael Croghan <mcrog.7@gmail.com>
2023-12-31 15:40:17 -0500

    implement findHappyNumber (with & without HashSet)

---
+++ src/patterns/fastslowpointers/HappyNumber.java
@@ -0,0 +1,42 @@
+package patterns.fastslowpointers;
+
+import java.util.HashSet;
+
+public class HappyNumber {
+
+
+    public static boolean findHappyNumber(int num) {
+        int slow = num, fast = num;
+        do {
+            slow = calculateSquaredSum(slow);
+            fast = calculateSquaredSum(calculateSquaredSum(fast));
+        } while (slow != fast);
+        return slow == 1;
+    }
+
+    private static int calculateSquaredSum(int num) {
+        int squaredSum = 0;
+        while (num > 0) {
+            int currentDigit = num % 10;
+            squaredSum += (currentDigit * currentDigit);
+            num /= 10;
+        }
+        System.out.println(squaredSum);
+        return squaredSum;
+    }
+
+    public static boolean bruteForceFindHappy(int num) {
+        HashSet<Integer> nums = new HashSet<>();
+        int currentNum = num;
+        while (currentNum != 1) {
+            if (nums.contains(currentNum)) {
+                return false;
+            }
+            else {
+                nums.add(currentNum);
+                currentNum = calculateSquaredSum(currentNum);
+            }
+        }
+        return true;
+    }
+}
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,12 +1,6 @@
 package scratchpad;
 
-import datastructures.linkedlists.ListNode;
-import patterns.fastslowpointers.CyclicLinkedList;
-import patterns.fastslowpointers.FindCycleStart;
-import patterns.fastslowpointers.MiddleLinkedList;
-
 public class PracticeClient {
-
 
     public static void main(String[] args) {
 
