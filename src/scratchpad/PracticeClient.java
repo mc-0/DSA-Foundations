ef3d220fe7f7e56c23d9d7b8b23c84b3006d47df
Michael Croghan <mcrog.7@gmail.com>
2023-12-31 14:41:44 -0500

    implement FindCycleStart

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -2,23 +2,14 @@ package scratchpad;
 
 import datastructures.linkedlists.ListNode;
 import patterns.fastslowpointers.CyclicLinkedList;
+import patterns.fastslowpointers.FindCycleStart;
 import patterns.fastslowpointers.MiddleLinkedList;
 
 public class PracticeClient {
 
 
     public static void main(String[] args) {
-        ListNode<Integer> n1 = new ListNode<>(1);
-        ListNode<Integer> n2 = new ListNode<>(2);
-        ListNode<Integer> n3 = new ListNode<>(3);
-        ListNode<Integer> n4 = new ListNode<>(4);
-        ListNode<Integer> n5 = new ListNode<>(5);
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        n4.next = n5;
-//        n5.next = n3;
-        System.out.println(MiddleLinkedList.findMiddle(n1).value);
+
 
     }
 
