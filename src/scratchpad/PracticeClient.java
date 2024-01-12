22be8a9fea4b2c72be7915ad56598023f5ea6bdf
Michael Croghan <mcrog.7@gmail.com>
2024-01-11 19:57:32 -0500

    Implement FindDuplicateNumber

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,6 +1,7 @@
 package scratchpad;
 
 import patterns.cyclicsort.FindAllMissingNumbers;
+import patterns.cyclicsort.FindDuplicateNumber;
 import patterns.cyclicsort.FindMissingNumber;
 
 import java.util.Arrays;
@@ -9,9 +10,9 @@ public class PracticeClient {
 
     public static void main(String[] args) {
 
-        int[] nums = {1, 3, 1, 8, 2, 3, 5, 1};
+        int[] nums = {2,1,3,3,5,4};
         System.out.println(Arrays.toString(nums));
-        System.out.println(FindAllMissingNumbers.find(nums));
+        System.out.println(FindDuplicateNumber.findNumber(nums));
         System.out.println(Arrays.toString(nums));
 
 
