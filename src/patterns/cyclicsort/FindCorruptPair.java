c0e922aa3895e67c8b209ffdc56feba18d4ab67f
Michael Croghan <mcrog.7@gmail.com>
2024-01-12 08:37:30 -0500

    Implement FindCorruptPair

---
+++ src/patterns/cyclicsort/FindCorruptPair.java
@@ -0,0 +1,24 @@
+package patterns.cyclicsort;
+
+import static patterns.util.Helper.swap;
+
+public class FindCorruptPair {
+
+    public static int[] findPairs(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+            else {
+                i++;
+            }
+        }
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] != k + 1) {
+                return new int[] {nums[k], k + 1};
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
