dd2af0314b3047277cefa591936fb3e522d22e16
Michael Croghan <mcrog.7@gmail.com>
2024-01-12 08:10:14 -0500

    Implement FindAllDuplicateNumbers

---
+++ src/patterns/cyclicsort/FindAllDuplicateNumbers.java
@@ -0,0 +1,32 @@
+package patterns.cyclicsort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static patterns.util.Helper.swap;
+
+public class FindAllDuplicateNumbers {
+
+    // Time: O(N)
+    // Space: O(1)
+    public static List<Integer> findNumbers(int[] nums) {
+        List<Integer> duplicateNumbers = new ArrayList<>();
+        int i = 0;
+        while (i < nums.length) {
+
+            if (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+            else {
+                i++;
+            }
+
+        }
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] != k + 1) {
+                duplicateNumbers.add(nums[k]);
+            }
+        }
+        return duplicateNumbers;
+    }
+}
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,5 +1,6 @@
 package scratchpad;
 
+import patterns.cyclicsort.FindAllDuplicateNumbers;
 import patterns.cyclicsort.FindAllMissingNumbers;
 import patterns.cyclicsort.FindDuplicateNumber;
 import patterns.cyclicsort.FindMissingNumber;
@@ -10,9 +11,9 @@ public class PracticeClient {
 
     public static void main(String[] args) {
 
-        int[] nums = {1,3,4,2,2};
+        int[] nums = {3, 4, 4, 5, 5};
         System.out.println(Arrays.toString(nums));
-        System.out.println(FindDuplicateNumber.findNumber(nums));
+        System.out.println(FindAllDuplicateNumbers.findNumbers(nums));
         System.out.println(Arrays.toString(nums));
 
 
