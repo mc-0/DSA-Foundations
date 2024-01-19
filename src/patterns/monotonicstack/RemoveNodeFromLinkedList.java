ab2e7bcb70ab5870695d2b3d56750ca8772fd378
Michael Croghan <mcrog.7@gmail.com>
2024-01-18 20:21:53 -0500

    Implement RemoveNodesFromLinkedList

---
+++ src/patterns/monotonicstack/RemoveNodeFromLinkedList.java
@@ -0,0 +1,27 @@
+package patterns.monotonicstack;
+
+import datastructures.linkedlists.ListNode;
+
+import java.util.List;
+import java.util.Stack;
+
+public class RemoveNodeFromLinkedList {
+
+    public static ListNode<Integer> removeNodes(ListNode<Integer> head) {
+        Stack<ListNode> s = new Stack<>();
+        ListNode current = head;
+
+        while (current != null) {
+            while (!s.isEmpty() && ((int) current.value > (int) s.peek().value)) {
+                s.pop();
+            }
+            if (!s.isEmpty()) {
+                s.peek().next = current;
+            }
+            s.push(current);
+            current = current.next;
+        }
+
+        return s.isEmpty() ? null : s.get(0);
+    }
+}
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -3,6 +3,7 @@ package scratchpad;
 import datastructures.linkedlists.ListNode;
 import patterns.cyclicsort.*;
 import patterns.monotonicstack.DailyTemperatures;
+import patterns.monotonicstack.RemoveNodeFromLinkedList;
 import patterns.reverselinkedlist.ReverseAlternatingKElementSubList;
 import patterns.reverselinkedlist.ReverseEveryKElementSubList;
 import patterns.reverselinkedlist.ReverseSubList;
@@ -13,9 +14,22 @@ import java.util.Arrays;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] nums = {70, 73, 75, 71, 69, 72, 76, 73};
-        System.out.println(Arrays.toString(DailyTemperatures.getTemps(nums)));
+        ListNode n1 = new ListNode(5);
+        ListNode n2 = new ListNode(3);
+        ListNode n3 = new ListNode(7);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(2);
+        ListNode n6 = new ListNode(1);
 
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = null;
+
+        ListNode answer = RemoveNodeFromLinkedList.removeNodes(n1);
+        Helper.printList(answer);
     }
 
 }
