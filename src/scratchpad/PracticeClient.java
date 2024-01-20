95e5199724d45f6cc473122d59abc325eeed4c90
Michael Croghan <mcrog.7@gmail.com>
2024-01-19 19:52:43 -0500

    Implement BalancedParatheses... again!

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -9,6 +9,7 @@ import patterns.monotonicstack.RemoveNodeFromLinkedList;
 import patterns.reverselinkedlist.ReverseAlternatingKElementSubList;
 import patterns.reverselinkedlist.ReverseEveryKElementSubList;
 import patterns.reverselinkedlist.ReverseSubList;
+import patterns.stacks.BalancedParentheses;
 import patterns.util.Helper;
 
 import java.util.Arrays;
@@ -16,14 +17,10 @@ import java.util.Arrays;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        String f = "abbbaaca";
-        int[] a = new int[f.length()];
-//        System.out.println(RemoveAdjacentDuplicatesII.remove(f, 3));
-        for (int i = 0; i < f.length(); i++) {
-            a[i] = f.charAt(i);
-        }
+        String f = "{[(])}";
 
-        System.out.println(Arrays.toString(a));
+
+        System.out.println(BalancedParentheses.checkIfBalanced(f));
     }
 
 }
