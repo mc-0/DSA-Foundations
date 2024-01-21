64a1718f8a95f65149ec7c763490f73c57262c1d
Michael Croghan <mcrog.7@gmail.com>
2024-01-20 21:20:39 -0500

    Impl SimplifyFilePath

---
+++ src/patterns/stacks/SimplifyFilePath.java
@@ -0,0 +1,29 @@
+package patterns.stacks;
+
+import java.util.Stack;
+
+public class SimplifyFilePath {
+
+    public static String simplify(String s) {
+        Stack<String> stack = new Stack<>();
+
+        for (String dir : s.split("/")) {
+            if (dir.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }
+            else if (!dir.isEmpty() && !dir.equals(".")){
+                stack.push(dir);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String str : stack) {
+            result.append("/");
+            result.append(str);
+        }
+
+        return result.length() == 0 ? "/" : result.toString();
+    }
+}
