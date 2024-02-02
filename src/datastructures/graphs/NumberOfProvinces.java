6f0541a8aab3801117902a452f13847dc63f2a43
Michael Croghan <mcrog.7@gmail.com>
2024-02-01 19:05:54 -0500

    Implement NumberOfProvinces

---
+++ src/datastructures/graphs/NumberOfProvinces.java
@@ -0,0 +1,27 @@
+package datastructures.graphs;
+
+// Time: O(n^2)
+// Space: O(n)
+public class NumberOfProvinces {
+    public static int findProvinces(int[][] isConnected) {
+        int numProvinces = 0; // Count the number of provinces to return
+        boolean[] visited = new boolean[isConnected.length]; // Each sub-array represents connections to other nodes
+
+        for (int i = 0; i < isConnected.length; i++) {
+            if (!visited[i]) {
+                recurseDFS(isConnected, visited, i);
+                numProvinces++;
+            }
+        }
+        return numProvinces;
+    }
+
+    private static void recurseDFS(int[][] isConnected, boolean[] visited, int i) {
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                recurseDFS(isConnected, visited, j);
+            }
+        }
+    }
+}
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -7,27 +7,7 @@ import datastructures.graphs.Pair;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        Graph graph = new Graph();
-        graph.addVertex(1);
-        graph.addVertex(2);
-        graph.addVertex(3);
-        graph.addVertex(4);
-        graph.addVertex(5);
-        graph.addVertex(6);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(2, 3);
-        graph.addEdge(3, 4);
-        graph.addEdge(4, 5);
-        graph.addEdge(5, 6);
 
-//        graph.DFS(2);
-        int[][] x = new int[3][2];
-        x[0] = new int[]{0, 1};
-        x[1] = new int[]{1, 2};
-        x[2] = new int[]{2, 3};
-        FindPathInGraph g = new FindPathInGraph();
-        System.out.println(g.hasPath(4, x, 0, 3));
     }
 
 
