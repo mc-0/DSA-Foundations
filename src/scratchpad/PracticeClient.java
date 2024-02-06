e989c018f7508191a86960bec8e18eb4b2020572
Michael Croghan <mcrog.7@gmail.com>
2024-02-06 05:22:13 -0500

    Implement DistinctIslands

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -6,13 +6,13 @@ import patterns.util.Helper;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[][] n = {{0,0,0,0,0},
+        int[][] n = {{0,1,0,1,1},
                 {0,0,0,0,0},
                 {0,0,1,1,0},
                 {0,0,1,0,0},
                 {0,0,0,0,0}};
 
-        System.out.println(IslandPerimeter.calculateIslandSize(n));
+        System.out.println(DistinctIslands.findDistinctIslands(n));
 
     }
 
