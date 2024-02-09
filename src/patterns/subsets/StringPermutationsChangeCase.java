24a60002b34358321a39492f79ffa780b801e2bf
Michael Croghan <mcrog.7@gmail.com>
2024-02-09 16:12:40 -0500

    Implement StringPermutationsChangeCase

---
+++ src/patterns/subsets/StringPermutationsChangeCase.java
@@ -0,0 +1,33 @@
+package patterns.subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringPermutationsChangeCase {
+
+    public static List<String> findLetterCaseStringPermutations(String str) {
+        List<String> permutations = new ArrayList<>();
+        if (str == null)
+            return permutations;
+
+        permutations.add(str);
+        // process every character of the string one by one
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) { // only process characters, skip digits
+                // we'll take all existing permutations and change the letter case appropriately
+                int n = permutations.size();
+                for (int j = 0; j < n; j++) {
+                    char[] chs = permutations.get(j).toCharArray();
+                    // if the current char is in upper case change it to lower case or vice versa
+                    if (Character.isUpperCase(chs[i]))
+                        chs[i] = Character.toLowerCase(chs[i]);
+                    else
+                        chs[i] = Character.toUpperCase(chs[i]);
+                    permutations.add(String.valueOf(chs));
+                }
+            }
+        }
+        return permutations;
+    }
+
+}
