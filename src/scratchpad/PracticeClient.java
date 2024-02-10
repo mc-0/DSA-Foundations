02c4f86b6bd0bbf5a76e960443d2b91a1435272d
Michael Croghan <mcrog.7@gmail.com>
2024-02-10 06:00:00 -0500

    Implement OrderAgnosticBinarySearch

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,6 +1,7 @@
 package scratchpad;
 
 import patterns.matrixtraversal.*;
+import patterns.modifiedbinarysearch.AgnosticOrderSearch;
 import patterns.subsets.FindPermutations;
 import patterns.subsets.FindSubsets;
 import patterns.twoheaps.MedianInNumberStream;
@@ -12,8 +13,8 @@ import java.util.PriorityQueue;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] n = {1,2,3};
-        System.out.println(FindPermutations.find(n));
+        int[] n = {48,32,22,13,10,9,4,2};
+        System.out.println(AgnosticOrderSearch.binarySearch(null, 100));
     }
 
 
