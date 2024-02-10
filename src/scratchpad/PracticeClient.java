ff021671bd42edb51823aa50e23e654afe15cf78
Michael Croghan <mcrog.7@gmail.com>
2024-02-10 06:25:10 -0500

    Implement CeilingOfNumber

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -2,6 +2,7 @@ package scratchpad;
 
 import patterns.matrixtraversal.*;
 import patterns.modifiedbinarysearch.AgnosticOrderSearch;
+import patterns.modifiedbinarysearch.CeilingOfNumber;
 import patterns.subsets.FindPermutations;
 import patterns.subsets.FindSubsets;
 import patterns.twoheaps.MedianInNumberStream;
@@ -13,8 +14,8 @@ import java.util.PriorityQueue;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] n = {48,32,22,13,10,9,4,2};
-        System.out.println(AgnosticOrderSearch.binarySearch(null, 100));
+        int[] n = {1,3,8,10,15};
+        System.out.println(CeilingOfNumber.findCeiling(n, 12));
     }
 
 
