90fb02228ccaf01c9332010f613a865f7e6aa1a1
Michael Croghan <mcrog.7@gmail.com>
2024-02-11 06:40:13 -0500

    Implement NextNumber

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -4,6 +4,7 @@ import patterns.matrixtraversal.*;
 import patterns.modifiedbinarysearch.AgnosticOrderSearch;
 import patterns.modifiedbinarysearch.CeilingOfNumber;
 import patterns.modifiedbinarysearch.FloorOfNumber;
+import patterns.modifiedbinarysearch.NextLetter;
 import patterns.subsets.FindPermutations;
 import patterns.subsets.FindSubsets;
 import patterns.twoheaps.MedianInNumberStream;
@@ -15,8 +16,9 @@ import java.util.PriorityQueue;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] n = {4,6,10};
-        System.out.println(FloorOfNumber.findFloor(n, -1));
+        char[] n = {'a', 'c', 'f', 'h'};
+        System.out.println(NextLetter.findNextLetter(n, 'f'));
+
     }
 
 
