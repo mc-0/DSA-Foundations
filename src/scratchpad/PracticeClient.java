db7d184406fe0208361bb6b554f971b3e6bad056
Michael Croghan <mcrog.7@gmail.com>
2024-02-12 15:56:10 -0500

    Implement CountRotations

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] n = {4, 5, 7, 9, 10, -1, 2};
-        System.out.println(SearchInRotatedArray.search(n, 10));
+        int[] n = {10, 15, 1, 3, 8};
+        System.out.println(RotationCount.countRotations(n));
 
     }
 
