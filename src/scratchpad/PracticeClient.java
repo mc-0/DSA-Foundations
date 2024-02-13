0e08e5e43b45f1871b61ce59e995911f8a8493d0
Michael Croghan <mcrog.7@gmail.com>
2024-02-13 16:58:52 -0500

    Implement TwoSingleNumbers

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,6 +1,7 @@
 package scratchpad;
 
 import patterns.bitwisexor.SingleNumber;
+import patterns.bitwisexor.TwoSingleNumbers;
 import patterns.modifiedbinarysearch.*;
 
 import java.util.Arrays;
@@ -8,7 +9,7 @@ import java.util.Arrays;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] n = {1, 4, 2, 1, 3, 5, 6, 2, 3, 5};
+        int[] n = {2,2,3,2};
         System.out.println(SingleNumber.findSingleNumber(n));
 
 
