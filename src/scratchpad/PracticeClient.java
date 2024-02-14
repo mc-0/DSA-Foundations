6ed49da025d9cbe3f1368c5599937151cacb3e34
Michael Croghan <mcrog.7@gmail.com>
2024-02-14 14:21:12 -0500

    Implement KClosestPointsToOrigin

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,19 +1,13 @@
 package scratchpad;
 
-import patterns.bitwisexor.ComplimentBaseTen;
-import patterns.bitwisexor.SingleNumber;
-import patterns.bitwisexor.TwoSingleNumbers;
-import patterns.modifiedbinarysearch.*;
-import patterns.topkelements.KthSmallestNumber;
-import patterns.topkelements.TopKNumbers;
-
-import java.util.Arrays;
+import patterns.topkelements.KClosestPointsToOrigin;
+import patterns.topkelements.Point;
 
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] n = {5, 12, 11, -1, 12};
-        System.out.println(KthSmallestNumber.findKthSmallestNumber(n, 3));
+        Point[] p = {new Point(1,2), new Point(1,3)};
+        System.out.println(KClosestPointsToOrigin.findClosestPoints(p,1));
     }
 
 
