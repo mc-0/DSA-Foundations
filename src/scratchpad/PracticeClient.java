6e44040ba857c400e0990403f0c9a642cb67e92d
Michael Croghan <mcrog.7@gmail.com>
2024-02-14 17:03:41 -0500

    Implement ConnectRopes

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,13 +1,17 @@
 package scratchpad;
 
+import patterns.topkelements.ConnectRopes;
 import patterns.topkelements.KClosestPointsToOrigin;
 import patterns.topkelements.Point;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class PracticeClient {
 
     public static void main(String[] args) {
-        Point[] p = {new Point(1,2), new Point(1,3)};
-        System.out.println(KClosestPointsToOrigin.findClosestPoints(p,1));
+        int[] n = {3, 4, 5, 6};
+        System.out.println(ConnectRopes.minimumCostToConnectRopes(n));
     }
 
 
