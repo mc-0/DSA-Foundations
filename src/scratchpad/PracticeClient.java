ff17b522b7778fb135828de066522444035bf5ab
Michael Croghan <mcrog.7@gmail.com>
2024-02-16 16:32:23 -0500

    Implement SumOfElements

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,11 +1,13 @@
 package scratchpad;
 
+import patterns.topkelements.SumOfElements;
 import patterns.topkelements.kclosestnumbers.KClosestNumbers;
 
 public class PracticeClient {
 
     public static void main(String[] args) {
-
+        int[] n = {3, 5, 8, 7};
+        System.out.println(SumOfElements.findSumOfElements(n, 1,4));
     }
 
 
