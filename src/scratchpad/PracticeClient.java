c4d89802f1cc332297a7f519e96e7478d1b551a0
Michael Croghan <mcrog.7@gmail.com>
2024-02-16 16:16:28 -0500

    Implement MaximumDistinctElements

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -5,10 +5,7 @@ import patterns.topkelements.kclosestnumbers.KClosestNumbers;
 public class PracticeClient {
 
     public static void main(String[] args) {
-//        [0,0,1,2,3,3,4,7,7,8] x: 3 k:5 -- Fails in LC
-        // Output: 347, should be
-        int[] n = {0,0,1,2,3,3,4,7,7,8};
-        System.out.println(KClosestNumbers.findClosestElements(n, 3, 5));
+
     }
 
 
