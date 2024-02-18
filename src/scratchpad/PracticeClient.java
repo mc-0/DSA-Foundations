f63e771976a103657f148f40c7b1ff12eb32cf6c
Michael Croghan <mcrog.7@gmail.com>
2024-02-18 13:08:43 -0500

    Implement ValidPalindromeII

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,5 +1,6 @@
 package scratchpad;
 
+import patterns.greedyalgorithms.ValidPalindromeII;
 import patterns.kwaymerge.KSmallestNumberInMSortedLists;
 import patterns.topkelements.SumOfElements;
 import patterns.topkelements.kclosestnumbers.KClosestNumbers;
@@ -11,15 +12,7 @@ import java.util.List;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        List<List<Integer>> n = new ArrayList<>();
-        List<Integer> l1 = Arrays.asList(2,6,8);
-        List<Integer> l2 = Arrays.asList(3,6,7);
-        List<Integer> l3 = Arrays.asList(1,3,4);
-        n.add(l1);
-        n.add(l2);
-        n.add(l3);
-        KSmallestNumberInMSortedLists k = new KSmallestNumberInMSortedLists();
-        System.out.println(k.findKthSmallest(n, 3));
+        System.out.println(ValidPalindromeII.isPalindromePossible("abcdef"));
     }
 
 
