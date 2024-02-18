36aa88bba3cb07ff267cfd748b27c8cecafbd97c
Michael Croghan <mcrog.7@gmail.com>
2024-02-18 05:47:34 -0500

    Implement KSmallestNumberInSortedLists

---
+++ src/patterns/kwaymerge/KSmallestNumberInMSortedLists.java
@@ -0,0 +1,46 @@
+package patterns.kwaymerge;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+// Time: O(K∗logM)
+// Space: O(M) - storing 1 element from each M input list
+public class KSmallestNumberInMSortedLists {
+    class Node {
+        int elementIndex;
+        int arrayIndex;
+
+        Node(int elementIndex, int arrayIndex) {
+            this.elementIndex = elementIndex;
+            this.arrayIndex = arrayIndex;
+        }
+    }
+
+    public int findKthSmallest(List<List<Integer>> lists, int k) {
+        PriorityQueue<Node> minHeap = new PriorityQueue<>(
+                Comparator.comparingInt(a -> lists.get(a.arrayIndex).get(a.elementIndex))
+        );
+
+        // insert 1st element of each list:
+        for (int i = 0; i < lists.size(); i++) {
+            if (!lists.get(i).isEmpty()) {
+                minHeap.add(new Node(0, i));
+            }
+        }
+
+        int numCount = 0, result = 0;
+        while (!minHeap.isEmpty()) {
+            Node node = minHeap.poll();
+            result =lists.get(node.arrayIndex).get(node.elementIndex);
+            if (++numCount == k) {
+                break;
+            }
+            node.elementIndex++; // point to the next index in the array
+            if (node.elementIndex < lists.get(node.arrayIndex).size()) {
+                minHeap.add(node);
+            }
+        }
+        return result;
+    }
+
+}
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,13 +1,25 @@
 package scratchpad;
 
+import patterns.kwaymerge.KSmallestNumberInMSortedLists;
 import patterns.topkelements.SumOfElements;
 import patterns.topkelements.kclosestnumbers.KClosestNumbers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PracticeClient {
 
     public static void main(String[] args) {
-        int[] n = {3, 5, 8, 7};
-        System.out.println(SumOfElements.findSumOfElements(n, 1,4));
+        List<List<Integer>> n = new ArrayList<>();
+        List<Integer> l1 = Arrays.asList(2,6,8);
+        List<Integer> l2 = Arrays.asList(3,6,7);
+        List<Integer> l3 = Arrays.asList(1,3,4);
+        n.add(l1);
+        n.add(l2);
+        n.add(l3);
+        KSmallestNumberInMSortedLists k = new KSmallestNumberInMSortedLists();
+        System.out.println(k.findKthSmallest(n, 3));
     }
 
 
