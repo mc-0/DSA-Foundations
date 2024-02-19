ee225177614317cf5a251a1c2c17c5819b809bef
Michael Croghan <mcrog.7@gmail.com>
2024-02-19 05:05:03 -0500

    Implement RemoveDuplicateLetters

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,15 +1,12 @@
 package scratchpad;
 
 import patterns.greedyalgorithms.MinAddToMakeParenthesesValid;
-import patterns.greedyalgorithms.ValidPalindromeII;
-
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import patterns.greedyalgorithms.RemoveDuplicateLetters;
 
 public class PracticeClient {
 
     public static void main(String[] args) {
-        System.out.println(MinAddToMakeParenthesesValid.minAddToMakeValid("(()())("));
+        System.out.println(RemoveDuplicateLetters.removeDuplicateLetters("mnopmn"));
     }
 
 
