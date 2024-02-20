4835fe1d43b708289f50aca3d44d8353f1bc3559
Michael Croghan <mcrog.7@gmail.com>
2024-02-20 05:16:43 -0500

    Implement KnapsackBasic

---
+++ src/patterns/knapsackdp/KnapsackBasic.java
@@ -0,0 +1,30 @@
+package patterns.knapsackdp;
+
+// Time: O(2^n)
+// Space: O(n)
+// This approach leads to overlapping sub-problems that are re-solved. Memoization can help solve the problem once
+public class KnapsackBasic {
+
+    public int solveKnapsack(int[] profits, int[] weights, int capacity) {
+        return this.knapsackRecursive(profits, weights, capacity, 0);
+    }
+
+    private int knapsackRecursive(int[] profits, int[] weights, int capacity, int currentIndex) {
+        // base checks
+        if (capacity <= 0 || currentIndex >= profits.length)
+            return 0;
+
+        // recursive call after choosing the element at the currentIndex
+        // if the weight of the element at currentIndex exceeds the capacity, we shouldn't process this
+        int profit1 = 0;
+        if( weights[currentIndex] <= capacity )
+            profit1 = profits[currentIndex] + knapsackRecursive(profits, weights,
+                    capacity - weights[currentIndex], currentIndex + 1);
+
+        // recursive call after excluding the element at the currentIndex
+        int profit2 = knapsackRecursive(profits, weights, capacity, currentIndex + 1);
+
+        return Math.max(profit1, profit2);
+    }
+
+}
