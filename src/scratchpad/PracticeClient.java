00124296573d376691eb84a020e258b3ad3a2983
Michael Croghan <mcrog.7@gmail.com>
2024-03-02 15:46:25 -0500

    Implement OrderedSet

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,18 +1,24 @@
 package scratchpad;
 
+import patterns.orderedset.OrderedSet;
+
+import java.util.Comparator;
+
 public class PracticeClient {
 
     public static void main(String[] args) {
-        String s = "string";
-        System.out.println(s);
+        OrderedSet os = new OrderedSet(Comparator.reverseOrder());
 
-        String t = pad(s, 8);
-        System.out.println(t);
+        os.add(5);
+        os.add(1);
+        os.add(2);
+
+        System.out.println(os.find_by_order(1)); // Find 2nd element in reverse order
+        System.out.println(os.order_of_key(3));
+
     }
 
-    public static String pad(String s, int n) {
-        return String.format("%" + n + "s", s);
-    }
+
 
     }
 
