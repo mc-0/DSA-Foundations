63c7acc8e3928c990aaa4268e97027a38c903796
Michael Croghan <mcrog.7@gmail.com>
2024-03-02 05:35:54 -0500

    Bipartite Graph

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -3,10 +3,16 @@ package scratchpad;
 public class PracticeClient {
 
     public static void main(String[] args) {
+        String s = "string";
+        System.out.println(s);
 
+        String t = pad(s, 8);
+        System.out.println(t);
     }
 
-
+    public static String pad(String s, int n) {
+        return String.format("%" + n + "s", s);
+    }
 
     }
 
