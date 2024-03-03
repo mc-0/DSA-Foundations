5b072651ff42df64c4039b9107c3b56fa2b1a15b
Michael Croghan <mcrog.7@gmail.com>
2024-03-03 10:30:55 -0500

    Implement MergeSimilarItems

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -7,14 +7,7 @@ import java.util.Comparator;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        OrderedSet os = new OrderedSet(Comparator.reverseOrder());
 
-        os.add(5);
-        os.add(1);
-        os.add(2);
-
-        System.out.println(os.find_by_order(1)); // Find 2nd element in reverse order
-        System.out.println(os.order_of_key(3));
 
     }
 
