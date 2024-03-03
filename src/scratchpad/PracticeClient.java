ba65653fee104edb06b289f8f96095ef7690d05e
Michael Croghan <mcrog.7@gmail.com>
2024-03-03 12:36:07 -0500

    Implement 132Pattern

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,12 +1,18 @@
 package scratchpad;
 
+import org.junit.jupiter.api.Order;
+import patterns.orderedset.OneThreeTwoPattern;
 import patterns.orderedset.OrderedSet;
 
 import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
 
 public class PracticeClient {
 
     public static void main(String[] args) {
+
+        System.out.println(Integer.MIN_VALUE);
 
 
     }
