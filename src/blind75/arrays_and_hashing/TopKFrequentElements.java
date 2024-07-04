7282d37e6c7f2fcd4e16a34833feae04566d5062
Michael Croghan <mcrog.7@gmail.com>
2024-07-04 19:52:11 -0400

    Add Top K Frequent Elements & Valid Palindrome

---
+++ src/blind75/arrays_and_hashing/TopKFrequentElements.java
@@ -0,0 +1,30 @@
+package blind75.arrays_and_hashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
+// 347. Top K Frequent Elements
+public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> hm.get(a) - hm.get(b));
+        for (int key : hm.keySet()) {
+            pq.add(key);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,5 +1,6 @@
 package scratchpad;
 
+import blind75.arrays_and_hashing.TopKFrequentElements;
 import blind75.arrays_and_hashing.ValidAnagram;
 import patterns.orderedset.MyCalendarI;
 
@@ -8,6 +9,9 @@ import java.util.*;
 public class PracticeClient {
 
     public static void main(String[] args) {
-
+        TopKFrequentElements topKFrequentElements = new TopKFrequentElements();
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequentElements.topKFrequent(nums, k)));
     }
 }
