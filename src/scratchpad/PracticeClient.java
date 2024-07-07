6acfe7c909fade4229c8d21a58fdb08d59821ca4
Michael Croghan <mcrog.7@gmail.com>
2024-07-07 18:59:28 -0400

    Implement 3Sum

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -2,6 +2,7 @@ package scratchpad;
 
 import blind75.arrays_and_hashing.TopKFrequentElements;
 import blind75.arrays_and_hashing.ValidAnagram;
+import blind75.twopointers.ThreeSum;
 import patterns.orderedset.MyCalendarI;
 
 import java.util.*;
@@ -9,9 +10,9 @@ import java.util.*;
 public class PracticeClient {
 
     public static void main(String[] args) {
-        TopKFrequentElements topKFrequentElements = new TopKFrequentElements();
-        int[] nums = {1, 1, 1, 2, 2, 3};
-        int k = 2;
-        System.out.println(Arrays.toString(topKFrequentElements.topKFrequent(nums, k)));
+        ThreeSum threeSum = new ThreeSum();
+        int[] nums = {-1,0,1,2,-1,-4};
+
+        System.out.println(threeSum.solve(nums));
     }
 }
