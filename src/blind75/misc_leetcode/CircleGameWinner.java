905839365fb0dc74df496e0ce91ae3e9d726c9f6
Michael Croghan <mcrog.7@gmail.com>
2024-07-08 19:38:23 -0400

    Implement Find the Winnder of the Circular Game

---
+++ src/blind75/misc_leetcode/CircleGameWinner.java
@@ -0,0 +1,35 @@
+package blind75.misc_leetcode;
+
+import datastructures.linkedlists.ListNode;
+// 1823. Find the Winner of the Circular Game
+public class CircleGameWinner {
+    public static int solve(int n, int k) {
+        if (n <= 1 || k == 0) {
+            return n;
+        }
+        int popK = k;
+        // create circular linked list
+        ListNode<Integer> head = new ListNode<>(1);
+        ListNode<Integer> current = head;
+        for (int i = 2; i <= n; i++) {
+            current.next = new ListNode<>(i);
+            current = current.next;
+        }
+
+        // make the linked list circular
+        current.next = head;
+
+        // remove every kth node until only one node is left
+        while (current.next != current) {
+            popK = k;
+            while (--popK > 0) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+
+        return current.value;
+
+
+    }
+}
