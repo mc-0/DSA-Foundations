e943bc2fa0b6fee4344fe5e55c02f9b880087d8e
Michael Croghan <mcrog.7@gmail.com>
2024-07-08 19:35:15 -0400

    Implement Container w/ Most Water

--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -3,6 +3,7 @@ package scratchpad;
 import blind75.arrays_and_hashing.TopKFrequentElements;
 import blind75.arrays_and_hashing.ValidAnagram;
 import blind75.twopointers.ThreeSum;
+import datastructures.linkedlists.ListNode;
 import patterns.orderedset.MyCalendarI;
 
 import java.util.*;
@@ -14,5 +15,35 @@ public class PracticeClient {
         int[] nums = {-1,0,1,2,-1,-4};
 
         System.out.println(threeSum.solve(nums));
+    }
+
+    public static int solve(int n, int k) {
+        if (n <= 1 || k == 0) {
+            return n;
+        }
+        int popK = k;
+        // create circular linked list
+        ListNode<Integer> head = new ListNode<>(1);
+        ListNode<Integer> current = head;
+        for (int i = 2; i <= n; i++) {
+            current.next = new ListNode<>(i);
+            current = current.next;
+        }
+
+        // make the linked list circular
+        current.next = head;
+
+        // remove every kth node until only one node is left
+        while (current.next != current) {
+            popK = k;
+            while (--popK > 0) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+
+        return current.value;
+
+
     }
 }
