d940a2ead50e072b8d041712909c46fa27091ab3
Michael Croghan <mcrog.7@gmail.com>
2024-07-29 19:43:02 -0400

    Implement 121. Best Time To Buy & Sell Stock

---
+++ src/blind75/slidingwindow/BuyAndSellStock.java
@@ -0,0 +1,24 @@
+package blind75.slidingwindow;
+
+// 121. Best Time To Buy & Sell Stock
+public class BuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int left = 0, right = 1;
+        int profit = 0;
+
+        while (right < prices.length) {
+            int gain;
+            // check if current gain is profitable
+            if (prices[left] < prices[right]) {
+                gain = prices[right] - prices[left];
+                profit = Math.max(profit, gain);
+            }
+            else {
+                // the transaction isn't profitable, meaning a lower price is found at the right pointer index
+                left = right;
+            }
+            right++;
+        }
+        return profit;
+    }
+}
--- src/scratchpad/PracticeClient.java
+++ src/scratchpad/PracticeClient.java
@@ -1,21 +1,13 @@
 package scratchpad;
 
 import blind75.arrays_and_hashing.ValidSudoku;
+import blind75.slidingwindow.BuyAndSellStock;
 
 public class PracticeClient {
 
     public static void main(String[] args) {
-        String[][] input = new String[][]{{"5","3",".",".","7",".",".",".","."}
-,{"6",".",".","1","9","5",".",".","."}
-,{".","9","8",".",".",".",".","6","."}
-,{"8",".",".",".","6",".",".",".","3"}
-,{"4",".",".","8",".","3",".",".","1"}
-,{"7",".",".",".","2",".",".",".","6"}
-,{".","6",".",".",".",".","2","8","."}
-,{".",".",".","4","1","9",".",".","5"}
-,{".",".",".",".","8",".",".","7","9"}};
-
-        ValidSudoku v = new ValidSudoku();
-        System.out.println(v.isValidSudoku(input));
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        BuyAndSellStock buyAndSellStock = new BuyAndSellStock();
+        System.out.println(buyAndSellStock.maxProfit(prices));
     }
 }
