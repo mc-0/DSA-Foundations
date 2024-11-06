2068b38b8d7713a624c7ed11542f3fbb05e18dcf
Michael Croghan <mcrog.7@gmail.com>
2024-11-06 18:20:13 -0500

    Update Dijkstra 3-way sort & binary heap implementations

--- src/sorting/SortingClient.java
+++ src/sorting/SortingClient.java
@@ -9,9 +9,9 @@ import java.util.Arrays;
 public class SortingClient {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Integer[] generic = {55, 12, 300, 21, 7};
-        QuickSelect<Integer> qs = new QuickSelect<>();
-        System.out.println(qs.select(generic, 2));
+        Integer[] generic = {55, 12, 300, 21, 7, 1, 35, 709,14,4,51};
+        DijkstraThreeWaySort.sort(generic);
+        System.out.println(Arrays.asList(generic));
 
     }
 
